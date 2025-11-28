03be9e3e7a65babd89e10e9d33ddfb42592cd390
tegardhani21@gmail.com <tegardhani21@gmail.com>
2025-11-28 21:02:48 +0700

    start5

--- main/main.java
+++ main/main.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class main {
     public static void main(String[] args) {
-        JFrame window = new JFrame("CaturUmgPbo");
+        JFrame window = new JFrame("CaturAhmadRacing");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         panelgame gp = new panelgame();
